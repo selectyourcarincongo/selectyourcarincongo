eeb04e64bf7036caec250eab2b35ba31dfc71d27
selectyourcarincongo <ljacquesalbert@gmail.com>
2026-04-04 18:35:52 +0100

    Update Navbar component to replace logo URL with '/assets/logo.png'.

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src='/assets/logo.png' alt='Logo' />
+      {/* Other navbar items */}
+    </nav>
+  );
+};
+
+export default Navbar;
